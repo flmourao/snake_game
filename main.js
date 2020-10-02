64261683c5d14b12908f4de5f2c721ba5329326f
flmourao <flmourao2015@gmail.com>
2020-10-02 07:00:56 -0300

    definir fim do game

--- main.js
+++ main.js
@@ -47,6 +47,13 @@ function initGame(){
   if (snake[0].y > 15 * box && direction == "up") snake[0].y = 0;
   if (snake[0].y < 0 && direction == "down") snake[0].y = 16 * box;
 
+  for(i = 1; i < snake.length; i++){
+    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+      clearInterval(jogo);
+      alert("Game Over");
+    }
+  }
+
   criarBG();
   createSnake();
   drawFood();
